563d0ba9233fa27c5a9ff2ddb7929598604dfa0e
Ekansh-Coder5 <78861687+Ekansh-Coder5@users.noreply.github.com>
2022-01-23 17:54:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+nosex = 0
+nosey = 0
+
 function preload() {
+    clown_image = loadImage("https://i.postimg.cc/WpXjGc43/imageedit-3-5942487835.png")
 }
 
 function setup() {
@@ -22,11 +26,14 @@ function gotPoses(results) {
         console.log(results)
         console.log("NOSE.X.CORDS=" + results[0].pose.nose.x);
         console.log("NOSE.Y.CORDS=" + results[0].pose.nose.y);
+        nosex = results[0].pose.nose.x-20
+        nosey = results[0].pose.nose.y
     }
 }
 
 function draw() {
     image(video, 0, 0, 300, 300)
+    image(clown_image, nosex, nosey, 60, 30)
 }
 
 function snap() {
